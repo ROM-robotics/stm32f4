5a81c5e80f1028583a592d886ce8b37a7a7d9755
ROM Robotics <server01.psa1981@gmail.com>
2023-12-09 10:24:22 +0630

    hal

--- Keil/clockExercise/main.c
+++ Keil/clockExercise/main.c
@@ -4,13 +4,12 @@
 int main()
 {
 	// enable adc1 clock
-	RCC->APB2ENR |= 1<<8;
+	RCC->APB2ENR |= 1 << 8;
 	ADC_TypeDef *pADC;
 	pADC = ADC1;
 	
-	
-	
 	pADC->CR1 = 0x55;
+	
 	
 	return 0;
 }
---
+++ cubeMX/f3discoveryBlink/ledblink/MDK-ARM/ledblink/startup_stm32f303xc.d
@@ -0,0 +1,1 @@
+ledblink\startup_stm32f303xc.o: startup_stm32f303xc.s
