2f325a2c3b5b8d0a36b95f6ed8c7d0700b54f543
ROM Robotics <server01.psa1981@gmail.com>
2024-01-23 08:11:18 +0630

    accelero update

--- STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/03 spi motion  3-Axis accelerometer/Core/Inc/main.h
+++ STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/03 spi motion  3-Axis accelerometer/Core/Inc/main.h
@@ -57,8 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define MEMS_CS_Pin GPIO_PIN_3
-#define MEMS_CS_GPIO_Port GPIOE
 
 /* USER CODE BEGIN Private defines */
 
