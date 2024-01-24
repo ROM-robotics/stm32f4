c4a53f325d409fe4e4f35db49cbd08bf15c69749
ROM Robotics <server01.psa1981@gmail.com>
2024-01-24 21:05:17 +0630

    update

--- STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/03 spi motion  3-Axis accelerometer/Core/Src/main.c
+++ STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/03 spi motion  3-Axis accelerometer/Core/Src/main.c
@@ -45,6 +45,9 @@ I2C_HandleTypeDef hi2c1;
 SPI_HandleTypeDef hspi1;
 
 /* USER CODE BEGIN PV */
+/*#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2)); //set CP10 and CP11 Full Access 
+#endif */
 
 /* USER CODE END PV */
 
