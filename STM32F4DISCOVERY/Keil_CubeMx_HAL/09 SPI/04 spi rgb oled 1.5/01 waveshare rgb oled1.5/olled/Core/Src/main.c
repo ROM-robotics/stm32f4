86643f9c1bfc4bc714571359537ebc8fd7cac2ea
ROM Robotics <server01.psa1981@gmail.com>
2024-02-28 15:04:13 +0630

    update

--- STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/04 spi rgb oled 1.5/01 waveshare rgb oled1.5/olled/Core/Src/main.c
+++ STM32F4DISCOVERY/Keil_CubeMx_HAL/09 SPI/04 spi rgb oled 1.5/01 waveshare rgb oled1.5/olled/Core/Src/main.c
@@ -11,7 +11,7 @@
   *
   * This software is licensed under terms that can be found in the LICENSE file
   * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * If no LICENSE file it is provided AS-IS.
   *
   ******************************************************************************
   */
@@ -23,7 +23,11 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ssd1351.h"
+#include "fonts.h"
+#include "testimg.h"
+#include <string.h>
+#include <stdarg.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -65,7 +69,6 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -89,15 +92,18 @@ int main(void)
   MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
+	SSD1351_Init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	 // SSD1351_FillScreen(SSD1351_MAGENTA);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  loop();
   }
   /* USER CODE END 3 */
 }
@@ -148,7 +154,72 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void loop() {
+    // Check border
+    SSD1351_FillScreen(SSD1351_BLACK);
 
+    for(int x = 0; x < SSD1351_WIDTH; x++) {
+        SSD1351_DrawPixel(x, 0, SSD1351_RED);
+        SSD1351_DrawPixel(x, SSD1351_HEIGHT-1, SSD1351_RED);
+    }
+
+    for(int y = 0; y < SSD1351_HEIGHT; y++) {
+        SSD1351_DrawPixel(0, y, SSD1351_RED);
+        SSD1351_DrawPixel(SSD1351_WIDTH-1, y, SSD1351_RED);
+    }
+
+    HAL_Delay(3000);
+
+    // Check fonts
+    SSD1351_FillScreen(SSD1351_BLACK);
+    SSD1351_WriteString(0, 0, "Font_7x10, red on black, lorem ipsum dolor sit amet", Font_7x10, SSD1351_RED, SSD1351_BLACK);
+    SSD1351_WriteString(0, 3*10, "Font_11x18, green, lorem ipsum", Font_11x18, SSD1351_GREEN, SSD1351_BLACK);
+    SSD1351_WriteString(0, 3*10+3*18, "Font_16x26, blue, lorem ipsum dolor sit amet", Font_16x26, SSD1351_BLUE, SSD1351_BLACK);
+
+    HAL_Delay(1000);
+    SSD1351_InvertColors(true);
+    HAL_Delay(1000);
+    SSD1351_InvertColors(false);
+
+    HAL_Delay(5000);
+
+    // Check colors
+    SSD1351_FillScreen(SSD1351_WHITE);
+    SSD1351_WriteString(0, 0, "WHITE", Font_11x18, SSD1351_BLACK, SSD1351_WHITE);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_BLUE);
+    SSD1351_WriteString(0, 0, "BLUE", Font_11x18, SSD1351_BLACK, SSD1351_BLUE);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_RED);
+    SSD1351_WriteString(0, 0, "RED", Font_11x18, SSD1351_BLACK, SSD1351_RED);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_GREEN);
+    SSD1351_WriteString(0, 0, "GREEN", Font_11x18, SSD1351_BLACK, SSD1351_GREEN);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_CYAN);
+    SSD1351_WriteString(0, 0, "CYAN", Font_11x18, SSD1351_BLACK, SSD1351_CYAN);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_MAGENTA);
+    SSD1351_WriteString(0, 0, "MAGENTA", Font_11x18, SSD1351_BLACK, SSD1351_MAGENTA);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_YELLOW);
+    SSD1351_WriteString(0, 0, "YELLOW", Font_11x18, SSD1351_BLACK, SSD1351_YELLOW);
+    HAL_Delay(500);
+
+    SSD1351_FillScreen(SSD1351_BLACK);
+    SSD1351_WriteString(0, 0, "BLACK", Font_11x18, SSD1351_WHITE, SSD1351_BLACK);
+    HAL_Delay(500);
+
+    SSD1351_DrawImage(0, 0, 128, 128, (const uint16_t*)test_img_128x128);
+
+    HAL_Delay(10000);
+}
 /* USER CODE END 4 */
 
 /**
